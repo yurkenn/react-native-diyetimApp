a7853b4e2d0145fa15eaa330e3fa4fcd177f4028
oguz yurken <oguz.yurken@gmail.com>
2022-08-15 03:33:46 +0300

    Update Navigator

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import {View, Text} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import SignIn from './pages/auth/SignIn';
 import SignUp from './pages/auth/SignUp';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen component={SignIn} name="SignInScreen" />
         <Stack.Screen component={SignUp} name="SignUpScreen" />
       </Stack.Navigator>
