c637d171ede81ca0e9d867082ceaf5f6601dc4db
oguz yurken <oguz.yurken@gmail.com>
2022-08-15 03:33:22 +0300

    Create SingUp Page

--- src/pages/auth/SignUp/SignUp.js
+++ src/pages/auth/SignUp/SignUp.js
@@ -1,10 +1,54 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import styles from './SignUp.style';
+import Button from '../../../components/Button';
+import Input from '../../../components/Input';
+import {Formik} from 'formik';
+const SignUp = ({navigation}) => {
+  // navigation
+  const handleBackButton = () => {
+    navigation.goBack();
+  };
+  const initialFormValues = {
+    usermail: '',
+    password: '',
+    repassword: '',
+  };
 
-const SignUp = () => {
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome</Text>
+      <Formik
+        initialValues={initialFormValues}
+        onSubmit={values => console.log(values)}>
+        {({handleChange, handleSubmit, values}) => (
+          <View>
+            <Input
+              onChangeText={handleChange('usermail')}
+              value={values.usermail}
+              placeholder="email..."
+              icon="email"
+            />
+            <Input
+              onChangeText={handleChange('password')}
+              value={values.password}
+              placeholder="password..."
+              isSecure
+              icon="lock"
+            />
+            <Input
+              onChangeText={handleChange('repassword')}
+              value={values.repassword}
+              placeholder="repassword..."
+              isSecure
+              icon="lock"
+            />
+
+            <Button text="Sign Up" onPress={handleSubmit} />
+          </View>
+        )}
+      </Formik>
+      <Button text="Back" onPress={handleBackButton} />
     </View>
   );
 };
--- src/pages/auth/SignUp/SignUp.style.js
+++ src/pages/auth/SignUp/SignUp.style.js
@@ -1,3 +1,18 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    textAlign: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  title: {
+    margin: 20,
+    textAlign: 'center',
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
