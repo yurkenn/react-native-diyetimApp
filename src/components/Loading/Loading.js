8418a4d6888bd0e8603f96a9e297899e2875f030
oguz yurken <oguz.yurken@gmail.com>
2022-08-16 04:59:47 +0300

    Create loadingcard

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,8 @@
+import {View, Text, ActivityIndicator} from 'react-native';
+import React from 'react';
+
+const Loading = () => {
+  return <ActivityIndicator size="large" />;
+};
+
+export default Loading;
