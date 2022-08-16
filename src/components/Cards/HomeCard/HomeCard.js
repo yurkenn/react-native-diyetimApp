19d8a73768d95364e5ef6aff78fdbf7b051d7ee9
oguz yurken <oguz.yurken@gmail.com>
2022-08-16 04:59:10 +0300

    Create homecard

---
+++ src/components/Cards/HomeCard/HomeCard.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const HomeCard = () => {
+  return (
+    <View>
+      <Text>HomeCard</Text>
+    </View>
+  );
+};
+
+export default HomeCard;
