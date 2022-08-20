392a85a0588cd3793a54de220d6473b6ceae58dd
oguz yurken <oguz.yurken@gmail.com>
2022-08-20 04:28:48 +0300

    Update Auth

--- src/pages/auth/SignIn/SignIn.js
+++ src/pages/auth/SignIn/SignIn.js
@@ -1,26 +1,41 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './SignIn.style';
 import Button from '../../../components/Button';
 import Input from '../../../components/Input';
 import {Formik} from 'formik';
+import auth from '@react-native-firebase/auth';
+
+const initialFormValues = {
+  usermail: '',
+  password: '',
+};
+
 const SignIn = ({navigation}) => {
+  const [loading, setLoading] = useState(false);
   // navigation
   const handleSingUpButton = () => {
     navigation.navigate('SignUpScreen');
   };
 
-  const initialFormValues = {
-    usermail: '',
-    password: '',
+  const handleFormSignIn = async formValues => {
+    try {
+      await auth().signInWithEmailAndPassword(
+        formValues.usermail,
+        formValues.password,
+      );
+      navigation.navigate('HomeScreen');
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome</Text>
-      <Formik
-        initialValues={initialFormValues}
-        onSubmit={values => console.log(values)}>
+      <Formik initialValues={initialFormValues} onSubmit={handleFormSignIn}>
         {({handleChange, handleSubmit, values}) => (
           <View>
             <Input
@@ -37,7 +52,7 @@ const SignIn = ({navigation}) => {
               icon="lock"
             />
 
-            <Button text="Login" onPress={handleSubmit} />
+            <Button text="Login" loading={loading} onPress={handleSubmit} />
           </View>
         )}
       </Formik>
