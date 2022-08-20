d2408e336b2576ecfb4cb2540647172bbff1479e
oguz yurken <oguz.yurken@gmail.com>
2022-08-20 04:27:47 +0300

    Add Sing Out button

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,10 +1,27 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Button} from 'react-native';
+import React, {useEffect} from 'react';
+import auth from '@react-native-firebase/auth';
+const Profile = ({navigation}) => {
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+  function onAuthStateChanged(user) {
+    if (!user) {
+      navigation.navigate('SignInScreen');
+    }
+  }
 
-const Profile = () => {
+  const handleSignOut = () => {
+    auth()
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
+
   return (
     <View>
       <Text>Profile</Text>
+      <Button title="Sing Out" onPress={handleSignOut} />
     </View>
   );
 };
