36512b1aa75830338f0926aef689adfd15d7e6f2
oguz yurken <oguz.yurken@gmail.com>
2022-08-20 04:28:32 +0300

    Update HomeCard

--- src/components/Cards/HomeCard/HomeCard.js
+++ src/components/Cards/HomeCard/HomeCard.js
@@ -1,12 +1,13 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-
-const HomeCard = () => {
+import {View, Text, Image} from 'react-native';
+import React, {memo} from 'react';
+import styles from './HomeCard.style';
+const HomeCard = ({branded}) => {
   return (
-    <View>
-      <Text>HomeCard</Text>
+    <View style={styles.container}>
+      <Text style={styles.food_name}>{branded.food_name}</Text>
+      <Image style={styles.image} source={{uri: branded.photo.thumb}} />
     </View>
   );
 };
 
-export default HomeCard;
+export default memo(HomeCard);
