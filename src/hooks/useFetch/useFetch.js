c0d5201f278935dcd5e4438ef1a4ecb07d601436
oguz yurken <oguz.yurken@gmail.com>
2022-08-20 04:29:30 +0300

    Fix Api

--- src/hooks/useFetch/useFetch.js
+++ src/hooks/useFetch/useFetch.js
@@ -8,7 +8,13 @@ const useFetch = url => {
 
   const fetchData = useCallback(async () => {
     try {
-      const {data: response} = await axios.get(url);
+      const {data: response} = await axios.get(url, {
+        headers: {
+          'x-app-id': 'eab7e237',
+          'x-app-key': 'ce3f08acd177decd14304e2dcb3df1b7',
+          'x-remote-user-id': '0',
+        },
+      });
       setData(response);
       setLoading(false);
     } catch (err) {
