0bb15c92995ef6e13bab208fb1ba26fed3e13c6a
oguz yurken <oguz.yurken@gmail.com>
2022-08-20 04:29:49 +0300

    Import HomeCard

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,28 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-
+import {View, Text, FlatList, Image} from 'react-native';
+import React, {useState} from 'react';
+import Config from 'react-native-config';
+import useFetch from '../../hooks/useFetch';
+import Loading from '../../components/Loading';
+import Input from '../../components/Input';
+import HomeCard from '../../components/Cards/HomeCard';
 const Home = () => {
+  const [text, setText] = useState('');
+  const {data, loading, error} = useFetch(
+    `${Config.API_URL}search/instant?query=${text}`,
+  );
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  const renderData = ({item}) => {
+    return <HomeCard branded={item} />;
+  };
+
   return (
     <View>
-      <Text>Home</Text>
+      <Input onChangeText={setText} placeholder="Search foods to log" />
+      <FlatList data={data.branded} renderItem={renderData} />
     </View>
   );
 };
