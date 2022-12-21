20e1fab114fc32b957da53541d0a8efa7f5928e1
oğuz yürken <oguz.yurken@gmail.com>
2022-12-21 18:50:19 +0300

    Delete All

--- src/App.js
+++ src/App.js
@@ -1,78 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-
-import SignIn from './pages/auth/SignIn';
-import SignUp from './pages/auth/SignUp';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Settings from './pages/Settings';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import auth from '@react-native-firebase/auth';
-import SearchFood from './pages/SearchFood';
-import Basket from './pages/Basket';
-import FoodDetail from './pages/FoodDetail';
-const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator();
+import {View, Text} from 'react-native';
+import React from 'react';
 
 const App = () => {
-  const [isSignedIn, setIsSignedIn] = useState();
-
-  useEffect(() => {
-    auth().onAuthStateChanged(user => {
-      setIsSignedIn(!!user);
-    });
-  }, []);
-
-  const HomeStack = () => {
-    return (
-      <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen
-          component={Home}
-          name="HomeScreen"
-          options={{
-            title: 'Home',
-            tabBarIcon: ({size}) => <Icon name={'home'} size={size} />,
-          }}
-        />
-        <Tab.Screen
-          component={Profile}
-          name="ProfileScreen"
-          options={{
-            title: 'Profile',
-            tabBarIcon: ({size}) => <Icon name={'person'} size={size} />,
-          }}
-        />
-        <Tab.Screen
-          component={Settings}
-          name="SettingsScreen"
-          options={{
-            title: 'Settings',
-            tabBarIcon: ({size}) => <Icon name={'settings'} size={size} />,
-          }}
-        />
-      </Tab.Navigator>
-    );
-  };
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        {!isSignedIn ? (
-          <>
-            <Stack.Screen component={SignIn} name="SignInScreen" />
-            <Stack.Screen component={SignUp} name="SignUpScreen" />
-          </>
-        ) : (
-          <>
-            <Stack.Screen component={HomeStack} name="HomeStack" />
-            <Stack.Screen component={SearchFood} name="SearchFood" />
-            <Stack.Screen component={FoodDetail} name="FoodDetail" />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>App</Text>
+    </View>
   );
 };
 
--- src/components/Loading/Loading.js
+++
@@ -1,8 +0,0 @@
-import {View, Text, ActivityIndicator} from 'react-native';
-import React from 'react';
-
-const Loading = () => {
-  return <ActivityIndicator size="large" />;
-};
-
-export default Loading;
