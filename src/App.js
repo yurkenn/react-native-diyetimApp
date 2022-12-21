b9ac6cb71b967de0f0988779133abdbe0b26a114
oğuz yürken <oguz.yurken@gmail.com>
2022-12-21 22:46:43 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-
+import Router from './navigation/Router';
 const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
+  return <Router />;
 };
 
 export default App;
