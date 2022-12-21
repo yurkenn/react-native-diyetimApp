d154fbf283144e568a94310991d3bf1f1ac097b7
oğuz yürken <oguz.yurken@gmail.com>
2022-12-21 22:52:54 +0300

    Create Sign Screen

---
+++ src/screens/auth/Sign/Sign.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import styles from './Sign.styles';
+
+const Sign = () => {
+  return (
+    <View>
+      <Text>Sign</Text>
+    </View>
+  );
+};
+
+export default Sign;
