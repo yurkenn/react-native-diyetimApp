fe73f6fad3ce93e4ec9206acaf064bed25a8e40a
oğuz yürken <oguz.yurken@gmail.com>
2022-12-24 23:30:16 +0300

    Add Firebase functions

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AuthStack from './AuthStack';
 import HomeStack from './HomeStack';
+import auth from '@react-native-firebase/auth';
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const [userSession, setUserSession] = useState();
+
+  useEffect(() => {
+    auth().onAuthStateChanged(user => {
+      setUserSession(!!user);
+    });
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="AuthStack" component={AuthStack} />
-        <Stack.Screen name="HomeStack" component={HomeStack} />
+        {!userSession ? (
+          <Stack.Screen name="AuthStack" component={AuthStack} />
+        ) : (
+          <Stack.Screen name="HomeStack" component={HomeStack} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/auth/Login/Login.js
+++ src/screens/auth/Login/Login.js
@@ -1,11 +1,14 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import * as Yup from 'yup';
 import {Formik} from 'formik';
 import Input from '../../../components/Input';
 import Button from '../../../components/Button';
 import styles from './Login.styles';
+import {loginFB} from '../../../firebase';
 const Login = ({navigation}) => {
+  const [loading, setLoading] = useState(false);
+
   const validationSchema = Yup.object().shape({
     email: Yup.string().email('Invalid email').required('Required'),
     password: Yup.string().required('Required'),
@@ -13,12 +16,21 @@ const Login = ({navigation}) => {
   const handleSignUp = () => {
     navigation.navigate('SignUp');
   };
+  const onSubmit = async values => {
+    const {email, password} = values;
+    setLoading(true);
+    const result = await loginFB(email, password);
+    if (result) {
+      navigation.navigate('Home');
+    }
+    setLoading(false);
+  };
 
   return (
     <View style={styles.container}>
       <Formik
         initialValues={{email: '', password: ''}}
-        onSubmit={values => console.log(values)}
+        onSubmit={onSubmit}
         validationSchema={validationSchema}>
         {({handleChange, handleBlur, handleSubmit, values, errors}) => (
           <View style={styles.innerContainer}>
@@ -42,7 +54,7 @@ const Login = ({navigation}) => {
             {errors.password && (
               <Text style={styles.error}>{errors.password}</Text>
             )}
-            <Button title="Login" onPress={handleSubmit} />
+            <Button title="Login" onPress={handleSubmit} loading={loading} />
             <View style={styles.bottomContainer}>
               <Text style={styles.bottomText}>Don't You Have An Account ?</Text>
               <Text style={styles.bottomButton} onPress={handleSignUp}>
