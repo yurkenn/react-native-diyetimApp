460adb22260b37ef08c650c4b15c8fb8a8bb04c7
oğuz yürken <oguz.yurken@gmail.com>
2022-12-24 23:29:26 +0300

    Add indicator

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,20 @@
-import {TouchableOpacity, Text} from 'react-native';
+import {TouchableOpacity, Text, View, ActivityIndicator} from 'react-native';
 import React from 'react';
 import styles from './Button.style';
 
-const Button = ({title, onPress}) => {
+const Button = ({title, onPress, loading}) => {
   return (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
-      <Text style={styles.text}>{title}</Text>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={onPress}
+      disabled={loading}>
+      {loading ? (
+        <ActivityIndicator size="small" color="white" />
+      ) : (
+        <View>
+          <Text style={styles.text}>{title}</Text>
+        </View>
+      )}
     </TouchableOpacity>
   );
 };
