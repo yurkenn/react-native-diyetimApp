00fb1d59d23064e2b19c78668fe754b4cd7e6e02
oğuz yürken <oguz.yurken@gmail.com>
2022-12-24 21:29:07 +0300

    Add Provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import {View, Text} from 'react-native';
 import React from 'react';
 import Router from './navigation/Router';
+import store from './store/store';
+import {Provider} from 'react-redux';
+
 const App = () => {
-  return <Router />;
+  return (
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  );
 };
 
 export default App;
