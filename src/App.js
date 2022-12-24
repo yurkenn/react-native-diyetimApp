0c4d30d7d82d846dcfdab93466c8bf5383ad2f75
oğuz yürken <oguz.yurken@gmail.com>
2022-12-24 23:29:01 +0300

    Add flash message

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Router from './navigation/Router';
 import store from './store/store';
 import {Provider} from 'react-redux';
-
+import FlashMessage from 'react-native-flash-message';
 const App = () => {
   return (
     <Provider store={store}>
       <Router />
+      <FlashMessage position="top" />
     </Provider>
   );
 };
