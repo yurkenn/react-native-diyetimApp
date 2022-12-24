05b240e4dbe16f2d66723fd5d1f6672eacf6886b
oğuz yürken <oguz.yurken@gmail.com>
2022-12-24 22:37:43 +0300

    Update Login implement formik

--- src/screens/auth/Login/Login.js
+++ src/screens/auth/Login/Login.js
@@ -1,11 +1,52 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import * as yup from 'yup';
+import {Formik} from 'formik';
+import Input from '../../../components/Input';
+import Button from '../../../components/Button';
 import styles from './Login.styles';
-
 const Login = () => {
+  const validationSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().min(6).required(),
+  });
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={values => console.log(values)}
+        validationSchema={validationSchema}>
+        {({handleChange, handleBlur, handleSubmit, values, errors}) => (
+          <View style={styles.innerContainer}>
+            <Text style={styles.title}>kcal</Text>
+            <Input
+              placeholder="Email"
+              onChangeText={handleChange('email')}
+              handleBlur={handleBlur('email')}
+              value={values.email}
+              icon="email"
+            />
+            {errors.email && <Text style={styles.error}>{errors.email}</Text>}
+            <Input
+              placeholder="Password"
+              onChangeText={handleChange('password')}
+              handleBlur={handleBlur('password')}
+              value={values.password}
+              isSecure
+              icon="lock"
+            />
+            {errors.password && (
+              <Text style={styles.error}>{errors.password}</Text>
+            )}
+            <Button title="Login" onPress={handleSubmit} />
+            <View style={styles.bottomContainer}>
+              <Text style={styles.bottomText}>Don't you have an account ?</Text>
+              <Text style={styles.bottomButton}>Sign up</Text>
+            </View>
+          </View>
+        )}
+      </Formik>
     </View>
   );
 };
--- src/screens/auth/Login/Login.styles.js
+++ src/screens/auth/Login/Login.styles.js
@@ -1,9 +1,35 @@
 import {StyleSheet} from 'react-native';
+import colors from '../../../styles/colors';
 
-export const styles = StyleSheet.create({
+export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: colors.grey200,
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  innerContainer: {},
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: colors.lightGreen,
+    marginBottom: 50,
+  },
+  error: {
+    marginLeft: 30,
+    color: colors.orange,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  bottomText: {
+    color: colors.grey400,
+  },
+  bottomButton: {
+    color: colors.lightGreen,
+    marginLeft: 5,
   },
 });
