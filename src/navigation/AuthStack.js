d5eb225f8237fba956242c13a6e8080d75f4b0ca
oğuz yürken <oguz.yurken@gmail.com>
2022-12-25 22:28:31 +0300

    Add GetStarted Screen

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from '../screens/auth/Login';
 import Sign from '../screens/auth/Sign';
+import GetStarted from '../screens/auth/GetStarted/GetStarted';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ const AuthStack = () => {
       screenOptions={{
         headerShown: false,
       }}>
+      <Stack.Screen name="GetStarted" component={GetStarted} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="SignUp" component={Sign} />
     </Stack.Navigator>
