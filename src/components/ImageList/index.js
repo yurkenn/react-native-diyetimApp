91b8898a7eded88437b880a1287d40ab3c1cd661
oğuz yürken <oguz.yurken@gmail.com>
2022-12-25 22:28:20 +0300

    Add Images

---
+++ src/components/ImageList/index.js
@@ -0,0 +1,7 @@
+const Images = {
+  first: require('../../assets/images/pic1.png'),
+  second: require('../../assets/images/pic2.png'),
+  third: require('../../assets/images/pic3.png'),
+};
+
+export default Images;
