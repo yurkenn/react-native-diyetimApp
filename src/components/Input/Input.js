1974cc9d9538a95b6902938f422aa6182b1019e5
oğuz yürken <oguz.yurken@gmail.com>
2022-12-26 19:40:55 +0300

    Add onpress prop

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,7 +2,7 @@ import {View, TextInput} from 'react-native';
 import React from 'react';
 import styles from './Input.style';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-const Input = ({placeholder, onChangeText, value, isSecure, icon}) => {
+const Input = ({placeholder, onChangeText, value, isSecure, icon, onPress}) => {
   return (
     <View style={styles.container}>
       <TextInput
@@ -12,7 +12,7 @@ const Input = ({placeholder, onChangeText, value, isSecure, icon}) => {
         onChangeText={onChangeText}
         secureTextEntry={isSecure}
       />
-      <Icon name={icon} size={25} />
+      <Icon onPress={onPress} name={icon} size={25} />
     </View>
   );
 };
