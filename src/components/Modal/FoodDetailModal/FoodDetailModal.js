24286e67aaab8e3bc94d81f1cd310d938a436a22
oğuz yürken <oguz.yurken@gmail.com>
2023-01-02 19:51:21 +0300

    Add Close X icon

--- src/components/Modal/FoodDetailModal/FoodDetailModal.js
+++ src/components/Modal/FoodDetailModal/FoodDetailModal.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Modal from 'react-native-modal';
 import styles from './FoodDetailModal.styles';
 import Button from '../../Button';
+import Icon from 'react-native-vector-icons/FontAwesome';
 const FoodDetailModal = ({
   visible,
   onClose,
@@ -23,25 +24,33 @@ const FoodDetailModal = ({
       swipeDirection="down"
       style={styles.modal}>
       <View style={styles.container}>
+        <Icon
+          name="close"
+          size={30}
+          color="black"
+          onPress={onClose}
+          style={styles.close}
+        />
         <View style={styles.input_container}>
           <Image style={styles.image} source={{uri: item.food.image}} />
+          <Text style={styles.label}>{item.food.label}</Text>
           <View style={styles.nutrition_container}>
             <View style={styles.nutrition_inner_container}>
               <Text style={styles.nutrition_text}>Protein</Text>
-              <Text style={styles.nutrition_numbers}>{protein} g</Text>
+              <Text style={styles.nutrition_numbers}>{protein}g</Text>
             </View>
             <View style={styles.nutrition_inner_container}>
               <Text style={styles.nutrition_text}>Calories</Text>
-              <Text style={styles.nutrition_numbers}>{cal} Kcal</Text>
+              <Text style={styles.nutrition_numbers}>{cal}g</Text>
             </View>
             <View style={styles.nutrition_inner_container}>
               <Text style={styles.nutrition_text}>Fat</Text>
-              <Text style={styles.nutrition_numbers}>{fat} g</Text>
+              <Text style={styles.nutrition_numbers}>{fat}g</Text>
             </View>
 
             <View style={styles.nutrition_inner_container}>
               <Text style={styles.nutrition_text}>Carbs</Text>
-              <Text style={styles.nutrition_numbers}>{carb} g</Text>
+              <Text style={styles.nutrition_numbers}>{carb}g</Text>
             </View>
           </View>
 
