754e4ce52322d4c81b1c5b62659cc0dffce41bd7
oğuz yürken <oguz.yurken@gmail.com>
2023-01-02 20:45:26 +0300

    Update Favorites screen

--- src/screens/Favorites/Favorites.js
+++ src/screens/Favorites/Favorites.js
@@ -1,10 +1,38 @@
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, Image} from 'react-native';
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {selectFavorites} from '../../store/favoriteSlice';
+import SearchCard from '../../components/Cards/SearchCard/SearchCard';
+import styles from './Favorites.styles';
 
 const Favorites = () => {
+  const favorites = useSelector(selectFavorites);
+  console.log(favorites);
+
+  const renderItem = ({item}) => {
+    return <SearchCard item={item} />;
+  };
+
+  const emtpyList = (
+    <View style={styles.emptyContainer}>
+      <Image
+        style={styles.image}
+        source={require('../../assets/images/noFood.png')}
+      />
+      <Text style={styles.emptyHeader}>No Foods Found</Text>
+      <Text style={styles.emptyText}>
+        You don’t save any food. Go ahead, search and save your favorite food
+      </Text>
+    </View>
+  );
+
   return (
-    <View>
-      <Text>Favorites</Text>
+    <View style={styles.container}>
+      {favorites.length === 0 ? (
+        emtpyList
+      ) : (
+        <FlatList data={favorites} renderItem={renderItem} />
+      )}
     </View>
   );
 };
--- src/screens/Favorites/Favorites.styles.js
+++ src/screens/Favorites/Favorites.styles.js
@@ -1,4 +1,5 @@
 import {StyleSheet} from 'react-native';
+import colors from '../../styles/colors';
 
 export default StyleSheet.create({
   container: {
@@ -6,5 +7,28 @@ export default StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    resizeMode: 'contain',
+    width: 100,
+    height: 100,
+  },
+  emptyContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  emptyHeader: {
+    fontSize: 24,
+    fontWeight: '500',
+    color: colors.grey500,
+    marginVertical: 10,
+    letterSpacing: -0.24,
+  },
+  emptyText: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: colors.grey500,
+    marginVertical: 10,
   },
 });
