a8318c14963c0c84937937b312188be87bbc024e
oğuz yürken <oguz.yurken@gmail.com>
2023-01-02 19:08:49 +0300

    Add modal in search card

--- src/components/Cards/SearchCard/SearchCard.js
+++ src/components/Cards/SearchCard/SearchCard.js
@@ -1,23 +1,61 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, Pressable, TouchableOpacity} from 'react-native';
 import React from 'react';
 import styles from './SearchCard.styles';
+import FoodDetailModal from '../../Modal/FoodDetailModal/FoodDetailModal';
+import {useState} from 'react';
 const SearchCard = ({item}) => {
+  const [isVisible, setIsVisible] = useState(false);
+
   let Cal;
+  let Carb;
+  let Fat;
+  let Protein;
+  let Fiber;
   if (item.food && item.food.nutrients) {
     Cal = item.food.nutrients.ENERC_KCAL;
+    Carb = item.food.nutrients.CHOCDF;
+    Fat = item.food.nutrients.FAT;
+    Protein = item.food.nutrients.PROCNT;
+    Fiber = item.food.nutrients.FIBTG;
   }
 
+  const handleModalToggle = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const handleModalSend = () => {
+    handleModalToggle();
+  };
+
+  const foodName =
+    item.food.label.length > 20
+      ? item.food.label.slice(0, 20) + '...'
+      : item.food.label;
+
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{uri: item.food.image}} />
+    <TouchableOpacity onPress={handleModalToggle}>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image style={styles.image} source={{uri: item.food.image}} />
+        </View>
+        <View style={styles.textContainer}>
+          {Cal && <Text style={styles.cal}>{Cal} Kcal</Text>}
+          <Text style={styles.label}>{foodName}</Text>
+          <Text style={styles.category}>{item.food.category}</Text>
+        </View>
+        <FoodDetailModal
+          visible={isVisible}
+          onClose={handleModalToggle}
+          onSend={handleModalSend}
+          item={item}
+          carb={Carb}
+          fat={Fat}
+          protein={Protein}
+          fiber={Fiber}
+          cal={Cal}
+        />
       </View>
-      <View style={styles.textContainer}>
-        {Cal && <Text style={styles.cal}>{Cal} Kcal</Text>}
-        <Text style={styles.label}>{item.food.label}</Text>
-        <Text style={styles.category}>{item.food.category}</Text>
-      </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
