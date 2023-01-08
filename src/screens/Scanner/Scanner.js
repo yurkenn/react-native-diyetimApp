2ff3a4d0a27a8efa5f52d3cc054d45894c00180d
oğuz yürken <oguz.yurken@gmail.com>
2023-01-08 22:24:52 +0300

    Update Scanner Add scan button

--- src/screens/Scanner/Scanner.js
+++ src/screens/Scanner/Scanner.js
@@ -1,10 +1,23 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 import styles from './Scanner.styles';
+import Button from '../../components/Button';
+import ImagePicker from 'react-native-image-crop-picker';
+
 const Scanner = () => {
+  const handleOpenCamera = () => {
+    ImagePicker.openCamera({
+      width: 300,
+      height: 400,
+      cropping: true,
+    }).then(image => {
+      console.log(image);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
+      <Button title="Scan Barcod" onPress={handleOpenCamera} />
     </View>
   );
 };
